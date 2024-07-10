467ca4d4f1d7c2b9d01f05d03dab8c73894e348c
Amanda Easter <me@amandaeaster.com>
2024-07-10 11:37:57 -0500

    Updates section added

--- src/app/components/travel/travel.component.ts
+++ src/app/components/travel/travel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as updatesData from '../../../assets/text/updates.json';
 
 @Component({
   selector: 'app-travel',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./travel.component.scss']
 })
 export class TravelComponent implements OnInit {
+  updates: any = updatesData;
+  updateIndex=this.updates.length-1;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updatesIndex(inc: 1 | -1){
+    this.updateIndex+=inc;
+    if(this.updateIndex>this.updates.length-1){
+      this.updateIndex=0;
+    }
+    else if(this.updateIndex<0){
+      this.updateIndex=this.updates.length-1;
+    }
   }
 
 }
