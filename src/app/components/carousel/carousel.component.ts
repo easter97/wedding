1877695e06a7287b79489d9f3235d2f47df0a410
Amanda Easter <me@amandaeaster.com>
2024-07-18 22:15:45 -0500

    Added mobile styling for home page, menu

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -10,10 +10,10 @@ import { interval } from 'rxjs';
 })
 export class CarouselComponent implements OnInit {
   images: string[] = [
-  "./assets/images/gallery/engagement-4.jpg",
-  "./assets/images/gallery/engagement-2.jpg",
-  "./assets/images/gallery/engagement-3.jpg",
-  "./assets/images/gallery/engagement-7.jpg",];
+  "https://i.pinimg.com/564x/6d/c2/64/6dc264af09bddb53c629fef88f8e1c4f.jpg",
+  "https://i.pinimg.com/736x/ec/c1/b9/ecc1b97e36755276fa36527caba91169.jpg",
+  "https://i.pinimg.com/564x/57/ac/0c/57ac0c539e20eb946ea1b1912891f40a.jpg",
+];
   currentIndex = 0;
 
   constructor() { }
