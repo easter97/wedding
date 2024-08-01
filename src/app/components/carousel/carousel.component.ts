463bae1a8ee42cfa997de29213798a54e8e5819d
Amanda Easter <me@amandaeaster.com>
2024-07-31 21:04:21 -0500

    Styling updates and new photos

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -10,9 +10,9 @@ import { interval } from 'rxjs';
 })
 export class CarouselComponent implements OnInit {
   images: string[] = [
-  "https://i.pinimg.com/564x/6d/c2/64/6dc264af09bddb53c629fef88f8e1c4f.jpg",
-  "https://i.pinimg.com/736x/ec/c1/b9/ecc1b97e36755276fa36527caba91169.jpg",
-  "https://i.pinimg.com/564x/57/ac/0c/57ac0c539e20eb946ea1b1912891f40a.jpg",
+  "./assets/images/AllieRyannPhoto-DallasWeddingPhotographer-2977.jpg",
+  "./assets/images/AllieRyannPhoto-DallasWeddingPhotographer-2567.jpg",
+  "./assets/images/AllieRyannPhoto-DallasWeddingPhotographer-3090.jpg",
 ];
   currentIndex = 0;
 
