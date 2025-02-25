e73a120d40e9a665dcbf6b164849da32b3f1f59a
Amanda Easter <me@amandaeaster.com>
2025-02-24 18:17:40 -0600

    Added tabs and content

--- src/app/components/wedding-party/tabs/tabs.component.ts
+++ src/app/components/wedding-party/tabs/tabs.component.ts
@@ -1,5 +1,6 @@
 import { Component, ContentChildren, QueryList } from '@angular/core';
 import { TabComponent } from './tab.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -8,14 +9,39 @@ import { TabComponent } from './tab.component';
 })
 export class TabsComponent {
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+  activeTab;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  // ngOnInit() {
+  //   // Read the fragment from the URL and set the active tab
+  //   this.route.fragment.subscribe(fragment => {
+  //     if (fragment) {
+  //       // this.activeTab.title = fragment;
+  //       this.activeTab = this.tabs.find(tab => tab.title == fragment);
+  //     }
+  //   });
+  // }
+
+  setActiveTab(tab: TabComponent) {
+    this.activeTab = tab;
+    this.router.navigate([], { fragment: tab.title.toLowerCase() }); // Update URL fragment
+  }
   
   // contentChildren are set
   ngAfterContentInit() {
-    // get all active tabs
-    let activeTabs = this.tabs.filter((tab)=>tab.active);
-    
+        // Read the fragment from the URL and set the active tab
+        this.route.fragment.subscribe(fragment => {
+          if (fragment) {
+            // this.activeTab.title = fragment;
+            this.activeTab = this.tabs.find(tab => tab.title.toLowerCase() == fragment);
+          }
+        });
     // if there is no active tab set, activate the first
-    if(activeTabs.length === 0) {
+    if(this.activeTab){
+      this.selectTab(this.activeTab);
+    }
+    else {
       this.selectTab(this.tabs.first);
     }
   }
@@ -26,5 +52,7 @@ export class TabsComponent {
     
     // activate the tab the user has clicked on.
     tab.active = true;
+
+    this.setActiveTab(tab);
   }
 }
--- src/app/components/wedding-party/wedding-party.component.ts
+++ src/app/components/wedding-party/wedding-party.component.ts
@@ -13,11 +13,12 @@ export class WeddingPartyComponent {
     {item: "Sunscreen", notes: "1 bottle liquid, 2-3 spray", events: ['Boat Day', 'General'], audience:['All']},
     {item: "Bug Spray", notes: "1", audience: ['All'], events: ["General"]},
     {item: "Tip Money", notes: "Your discretion, we recommend $30 in small bills for tipping housekeeping or bar tenders", events: ['General'], audience:['All']},
-    {item: "Makeup", notes: "", events: ['General'], audience:['Bridesmaids']},
-    {item: "Hair Styling Tools", notes: "", events: ['Rehearsal Dinner', 'Wedding Day'], audience:['Bridesmaids']},
+    {item: "Makeup", notes: "", events: ['General'], audience:['Women']},
+    {item: "Hair Styling Tools", notes: "", events: ['Rehearsal Dinner', 'Wedding Day'], audience:['Women']},
     {item: "Swim Coverup", notes: "", events: ['General'], audience:['All']},
     {item: "Bridesmaid Dress", notes: "", events: ['Wedding Day'], audience:['Bridesmaids']},
     {item: "Brown Loafers", quantity: "", events: ['Wedding Day'], audience:['Groomsman']},
+    {item: "Collared Shirt", quantity: "", events: ['General'], audience:['Men']},
   ]
   checklist = [
     {
@@ -63,12 +64,184 @@ export class WeddingPartyComponent {
       ]
     },
   ];
+  timeline = [
+    { 
+      date: "Tuesday, October 7", 
+      events: [
+        { time: 'Any Time', 
+          title: 'Bridal Party Arrives in Mexico', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+        { 
+          time: 'Evening', 
+          title: 'Drinks', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+        { 
+          time: 'Evening', 
+          title: 'Dinner', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+      ]
+    },
+    { 
+      date: "Wednesday, October 8", 
+      events: [
+        { time: 'Morning', 
+          title: 'Breakfast', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+        { 
+          time: 'Afternoon (Time TBD)', 
+          title: 'Boat Day', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+        { 
+          time: 'Evening (Time TBD)', 
+          title: 'Desires Night Club', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+      ]
+    },
+    { 
+      date: "Thursday, October 9", 
+      events: [
+        { time: 'Morning', 
+          title: 'Spa Day / Golf?', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+      ]
+    },
+    { 
+      date: "Friday, October 10", 
+      events: [
+        { time: 'Morning', 
+          title: 'Rehearsal', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+        { 
+          time: 'Evening', 
+          title: 'Rehearsal Dinner', 
+          description: `<p>Join us this evening as we welcome our guests to our wedding weekend. Get to know one another and make plans for the days to come!</p>`
+        },
+      ]
+    },
+    { 
+      date: "Saturday, October 11", 
+      events: [
+        { time: '11:30 AM', 
+          title: 'Bridesmaids Report to Bridal Suite', 
+          description: `<p>amanda's room</p>`
+        },
+        { 
+          time: '12:00 PM', 
+          title: "Bride's Makeup", 
+          description: `<p>makeup</p>`
+        },
+        { 
+          time: '1:00 PM', 
+          title: "Bride's Hair", 
+          description: `<p>Bridal hair</p>
+          <p>Getting ready photos begin</p>`
+        },
+        { 
+          time: '1:30 PM', 
+          title: "Groomsmen Get Dressed", 
+          description: `<p>report to somewhere</p>`
+        },
+        { 
+          time: '2:00 PM', 
+          title: "First Look with Groom", 
+          description: `<p>location</p>`
+        },
+        { 
+          time: '2:15 PM', 
+          title: "Bridal Party Portraits", 
+          description: `<p>location</p>`
+        },
+        { 
+          time: '3:00 PM', 
+          title: "Bride & Groom Portraits", 
+          description: `<p>location</p>`
+        },
+        { 
+          time: '3:45 PM', 
+          title: "Prepare for Ceremony", 
+          description: `<p>Relax</p>
+          <p>Hair & makeup touchups</p>
+          <p>Usher reports to beach to start seating guests</p>`
+        },
+        { 
+          time: '4:00 PM', 
+          title: "Ceremony Starts", 
+          description: `<p>location</p>`
+        },
+        { 
+          time: '5:00 PM', 
+          title: "Cocktail Hour Starts", 
+          description: `<p>location</p>`
+        },
+        { 
+          time: '6:00 PM', 
+          title: "Reception Starts", 
+          description: `<p>location</p>`
+        },
+      ]
+    },
+  ];
+
+  //default is start, we'll add logic if the date matches a date in the timeline
+  currentDay = this.timeline[0];
+  organizeBy = "default"
+  allEvents = [];
+  allAudiences = [];
 
   ngOnInit(){
     for(let item of this.checklist){
       item.percentage = this.getProgressPercentage(item.target);
     }
+
+    this.allEvents = [...new Set(this.packing
+      .map(item => item.events)
+      .reduce((acc, val) => acc.concat(val), [])
+    )].map(event => ({ name: event, selected: false }));
+    
+    this.allAudiences = [...new Set(this.packing
+      .map(item => item.audience)
+      .reduce((acc, val) => acc.concat(val), [])
+    )].map(audience => ({ name: audience, selected: false }));
+    
+
   }
+
+  getSelectedFilters() {
+    const selectedEvents = this.allEvents
+      .filter(event => event.selected)
+      .map(event => event.name);
+  
+    const selectedAudiences = this.allAudiences
+      .filter(audience => audience.selected)
+      .map(audience => audience.name);
+  
+    return { selectedEvents, selectedAudiences };
+  }
+
+  getFilteredPacking() {
+    const { selectedEvents, selectedAudiences } = this.getSelectedFilters();
+  
+    return this.packing.filter(item => {
+      // If no filters are selected, return everything
+      if (selectedEvents.length === 0 && selectedAudiences.length === 0) {
+        return true;
+      }
+  
+      const eventMatch = selectedEvents.length === 0 || item.events.some(event => selectedEvents.includes(event));
+      const audienceMatch = selectedAudiences.length === 0 || item.audience.some(aud => selectedAudiences.includes(aud));
+  
+      return eventMatch && audienceMatch;
+    });
+  }
+  
 
     getProgressPercentage(target): number {
       let startDate = new Date('2025-01-01');  // Adjust to your start date
