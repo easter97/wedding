57b1ed18f619eeb3fb750435ee1c21d15dea4161
Amanda Easter <me@amandaeaster.com>
2025-11-13 17:22:20 -0600

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RsvpComponent } from './components/rsvp/rsvp.component';
-import { AuthGuard } from './auth/auth.guard';
-import { PasswordComponent } from './components/password/password.component';
-import { HomeComponent } from './components/home/home.component';
 import { ContainerComponent } from './components/container/container.component';
 import { GiftListComponent } from './components/gift-list/gift-list.component';
-import { WeddingPartyComponent } from './components/wedding-party/wedding-party.component';
+
 
 const routes: Routes = [
-  { path: '*' , redirectTo:'/home', pathMatch:'full'},
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
   { path: 'registry', component: GiftListComponent},
   { path: 'home', component: ContainerComponent }
 ];
